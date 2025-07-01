6ce4158c9176c83462617a31ba8d0ba5c6c2cd61
Narayan Behera <146068913+lijubehera@users.noreply.github.com>
2025-07-01 20:19:00 +0530

    Add files via upload

---
+++ day8.js
@@ -0,0 +1,12 @@
+// Comparison Operators
+let a = 5;
+let b = 2;
+
+console.log("a == b:", a == b);     // false
+console.log("a != b:", a != b);     // true
+console.log("a === b:", a === b);   // false (value & type)
+console.log("a !== b:", a !== b);   // true
+console.log("a > b:", a > b);       // true
+console.log("a >= b:", a >= b);     // true
+console.log("a < b:", a < b);       // false
+console.log("a <= b:", a <= b);     // false
